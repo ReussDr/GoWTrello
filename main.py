b9f6ffca94e6af77093eead51f7be8ff190f451b
Derian Reuss <reussdr@yahoo.com>
2020-08-03 11:32:52 -0400

    Adding initial code work

---
+++ main.py
@@ -0,0 +1,102 @@
+import datetime
+import pytz
+import sys
+from trello import TrelloClient
+
+
+def get_gow_board(client):
+    for board in client.list_boards():
+        if board.name == "Gems of War Tasks":
+            return board
+    return client.add_board("Gems of War Tasks")
+
+
+def get_gow_list_todo(gow_board):
+    for trello_list in gow_board.list_lists():
+        if trello_list.name == "To Do":
+            return trello_list
+    return gow_board.add_list("To Do")
+
+
+def get_cards_to_create(day_to_create):
+    daily_due = day_to_create + datetime.timedelta(days=1)
+    cards = {"Daily Delve": [day_to_create, daily_due],
+             "Daily Honor": [day_to_create, daily_due],
+             "Daily Adventure Board": [day_to_create, daily_due],
+             "Daily Dungeon": [day_to_create, daily_due],
+             }
+    if day_to_create.weekday() == 1:
+        cards["Faction Assault"] = [day_to_create, daily_due]
+    if day_to_create.weekday() == 2:
+        cards["Pet Rescue"] = [day_to_create, daily_due]
+    if day_to_create.weekday() == 3:
+        cards["Class Challenge"] = [day_to_create, daily_due]
+
+    beginning_of_week = day_to_create - datetime.timedelta(days=day_to_create.weekday())
+    weekly_due = beginning_of_week + datetime.timedelta(days=7)
+    cards["Weekly PvP Tier 1"] = [beginning_of_week, weekly_due]
+    cards["Weekly Campaign"] = [beginning_of_week, weekly_due]
+    cards["Weekly World Event"] = [beginning_of_week, weekly_due]
+    cards["Weekly Guild Donation"] = [beginning_of_week, weekly_due]
+    cards["Weekly Seal Collection"] = [beginning_of_week, weekly_due]
+    return cards
+
+
+def print_cards_list(cards):
+    for card in cards:
+        print(card, cards[card])
+        for date in cards[card]:
+            print(date)
+
+
+def calculate_day():
+    now_time = datetime.datetime.now().astimezone()
+    reset_today = datetime.datetime.combine(datetime.date.today(),
+                                            datetime.time(7, 0, 0, 0),
+                                            pytz.UTC)
+    day_to_populate = datetime.date.today()
+    # now_time -= datetime.timedelta(hours=20)
+    if now_time <= reset_today:
+        day_to_populate -= datetime.timedelta(days=1)
+    return day_to_populate
+
+
+def main():
+    # Calculate the day, and retrieve a list of cards to add
+    day = calculate_day()
+    #day -= datetime.timedelta(days=1)
+    cards = get_cards_to_create(day)
+    print_cards_list(cards)
+
+    # Connect to Trello Client, and get gow board and list
+    client = TrelloClient(api_key=key, api_secret=token)
+    gow_board = get_gow_board(client)
+
+    # Remove cards with expired due dates, and build a list of existing cards
+    existing_cards = []
+    for card in gow_board.all_cards():
+        if card.due_date < datetime.datetime.now().astimezone():
+            card.delete()
+        else:
+            existing_cards.append(card.name)
+    #print(existing_cards)
+
+    # Get GoW to do list
+    todo_list = get_gow_list_todo(gow_board)
+    #print(todo_list)
+
+    # Add new cards, if they aren't already in the list
+    for card in cards:
+        card_name = card + " [" + str(cards[card][0]) + "]"
+        print(card_name)
+        if card_name not in existing_cards:
+            due_date = datetime.datetime.combine(cards[card][1],
+                                                 datetime.time(7, 0, 0, 0),
+                                                 pytz.UTC)
+            print(due_date)
+            todo_list.add_card(card + " [" + str(cards[card][0]) + "]",
+                               due=due_date.isoformat())
+
+
+if __name__ == '__main__':
+    sys.exit(main())
