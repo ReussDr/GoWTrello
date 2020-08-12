dcedb1068eaf328a4382e2a2eb8c30b829617bb1
Derian Reuss <reussdr@yahoo.com>
2020-08-12 11:33:35 -0400

    Changing to use config.ini file

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
+import configparser
 import datetime
 import pytz
+import os
 import sys
 from trello import TrelloClient
+
+
+def load_config_properties():
+    """
+    Read trello connection properties from the config file or environment variables
+
+    :return: Dictionary with trello connection parameters
+    """
+    # Read properties from trello_config.ini if it is available
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    if 'Trello API Keys' not in config:
+        config['Trello API Keys'] = {}
+    if 'Trello Board Settings' not in config:
+        config['Trello Board Settings'] = {}
+
+    # If Environment variables are set, they override the trello_config.json
+    if os.environ.get('TRELLO_API_KEY') is not None:
+        config['Trello API Keys']['trello_api_key'] = os.environ.get('TRELLO_API_KEY')
+    if os.environ.get('TRELLO_API_SECRET') is not None:
+        config['Trello API Keys']['trello_api_secret'] = os.environ.get('TRELLO_API_SECRET')
+    if os.environ.get('TRELLO_BOARD_ID') is not None:
+        config['Trello Board Settings']['trello_board_id'] = os.environ.get('TRELLO_BOARD_ID')
+
+    return config
 
 
 def get_gow_board(client):
@@ -68,14 +96,20 @@ def calculate_day():
 
 
 def main():
+    config = load_config_properties()
+    #for area in config:
+    #    print("[", area, "]")
+    #    for item in config[area]:
+    #        print(" ", item, config[area][item])
+
     # Calculate the day, and retrieve a list of cards to add
     day = calculate_day()
-    #day -= datetime.timedelta(days=1)
     cards = get_cards_to_create(day)
     print_cards_list(cards)
 
     # Connect to Trello Client, and get gow board and list
-    client = TrelloClient(api_key=key, api_secret=token)
+    client = TrelloClient(api_key=config['Trello API Keys']['trello_api_key'],
+                          api_secret=config['Trello API Keys']['trello_api_secret'])
     gow_board = get_gow_board(client)
 
     # Remove cards with expired due dates, and build a list of existing cards
